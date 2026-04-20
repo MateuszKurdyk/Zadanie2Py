0be9e7bf2725160a3fb40509de71964a6016d62d
Mateusz Kurdyk <matic@MacBook-Air-Mateusz.local>
2026-04-20 21:34:57 +0200

    dodanie klasy dostawa

--- zad2.py
+++ zad2.py
@@ -6,16 +6,32 @@ class Pizza:
         self.rodzaj = rodzaj;
         self.cena = cena;
 
-    def info(self):
+    def pizzaInfo(self):
         print(f"Nazwa: {self.nazwa}, Rodzaj: {self.rodzaj}, Cena: {self.cena}");
 
 pizza1 = Pizza("Margherita", "Wegetariańska", 20);
-pizza1.info();
+pizza1.pizzaInfo();
 
-# class Dostawa:
+class Dostawa:
+    def __init__(self, rodzaj, odleglosc, cenaKm):
+        self.rodzaj = rodzaj;
+        self.odleglosc = odleglosc;
+        self.cenaKm = cenaKm;
+    
+    def dostawaInfo(self):
+        print(f"Rodzaj: {self.rodzaj}, Odległość: {self.odleglosc} km, Cena za km: {self.cenaKm} zł");
+
+dostawa1 = Dostawa("NaMiejscu", 0, 0);
+dostawa1.dostawaInfo();
 
 
+class Dodatki:
+    def __init__(self, nazwa, cena):
+        self.nazwa = nazwa;
+        self.cena = cena;
 
-# class Dodatki:
+    def dodatkiInfo(self):
+        print(f"Nazwa: {self.nazwa}, Cemna: {self.cena} zł");
 
-
+dodatki1 = Dodatki("Ser", 5);
+dodatki1.dodatkiInfo();
